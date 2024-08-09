96281793261573bb50358c5efe4ea7495ae6219a
amr <amrkataia1234@gmail.com>
2024-08-09 19:54:39 +0300

    our team

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OurTeamComponent } from './our-team/our-team.component';
+import { ContactUSComponent } from './contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,OurTeamComponent,ContactUSComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
